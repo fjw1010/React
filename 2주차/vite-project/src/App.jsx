7028a355de163a3bf48c298ac957513d5a68062b
Hanso <167108402+fjw1010@users.noreply.github.com>
2024-05-28 20:18:48 +0900

    props

--- 2주차/vite-project/src/App.jsx
+++ 2주차/vite-project/src/App.jsx
@@ -1,37 +1,21 @@
 import React from "react";
 
 function App() {
-  return (
-    <div>
-      <h1>부모-자식 관계 테스트</h1>
-      <GrandFather />
-    </div>
-  )
+  return <GrandFather />;
 }
 function GrandFather() {
-  return(
-    <div>
-      <p>나는 할아버지 입니다.</p>
-      <Monter />
-    </div>
-  )
+  const name = "르탄"
+  return <Monter name={name} />;
 }
 
-function Monter() {
-  return (
-    <div>
-      <p>나는 엄마입니다.</p>
-      <Child />
-    </div>
-  )
+function Monter(props) {
+  const name = props.name
+  return <Child name={name}/>;
 }
 
-function Child() {
-  return (
-    <div>
-      <p>나는 자식입니다.</p>
-    </div>
-  )
+function Child(props) {
+  const name = props.name
+  return <div>{name}</div>
 }
 
 export default App;
